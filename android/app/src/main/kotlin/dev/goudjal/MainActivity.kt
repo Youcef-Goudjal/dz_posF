4924743f20cffaa9457c560080be4e33c8adceaf
Youcef-Goudjal <m.goudjal.y@gmail.com>
2023-01-01 15:55:29 +0100

    very good core init

---
+++ android/app/src/main/kotlin/dev/goudjal/MainActivity.kt
@@ -0,0 +1,6 @@
+package dev.goudjal.dz_pos
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:dz_pos/app/app.dart';
+import 'package:dz_pos/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
